a775f4fa72f59880cfec135a3a4bd1be348b0ea2
root <rutholuwafemi95@gmail.com>
2023-02-16 09:31:46 -0800

    printf

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -A program that prints the size of various types on the computer it is compiled and run on
+ * Return 0 (Success)
+ */
+int main(void)
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf(size of a char
